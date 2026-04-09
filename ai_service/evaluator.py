4d309ab6a05e77950de77d526c18780fc8477c6c
Manthan Kharote <kharotemanthan@gmail.com>
2026-04-10 00:39:06 +0530

    Delete ai_service/evaluator.py

--- ai_service/evaluator.py
+++
@@ -1,9 +0,0 @@
-# Grader functions required by OpenEnv Validator
-def grade_task_1(*args, **kwargs):
-    return 0.45
-
-def grade_task_2(*args, **kwargs):
-    return 0.65
-
-def grade_task_3(*args, **kwargs):
-    return 0.85
